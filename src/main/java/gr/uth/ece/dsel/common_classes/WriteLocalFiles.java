9240b18924539dc6f143d23040e79e31e8ba988d
panmout <takismoutafis@outlook.com>
2023-03-18 18:53:13 +0200

    First commit

---
+++ src/main/java/gr/uth/ece/dsel/common_classes/WriteLocalFiles.java
@@ -0,0 +1,28 @@
+package gr.uth.ece.dsel.common_classes;
+
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+
+public final class WriteLocalFiles
+{
+    public static void writeFile(String file, String content)
+    {
+        try
+        {
+            Formatter outputTextFile = new Formatter(file);
+            outputTextFile.format(content);
+            outputTextFile.close();
+        }
+        catch (FormatterClosedException formatterException)
+        {
+            System.err.println("Error writing to file, exiting");
+            System.exit(2);
+        }
+        catch (IOException ioException)
+        {
+            System.err.println("Error writing to file, exiting");
+            System.exit(3);
+        }
+    }
+}
